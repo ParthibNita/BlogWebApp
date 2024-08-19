31da070a425f3ae64528de58533de697f09bc8a4
Parthib Deb <debparthib0040@gmail.com>
2024-08-20 02:44:34 +0530

    Config store

---
+++ src/Features/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+    status:false,
+    userData:null
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status = true
+            state.userData = action.payload
+        },
+
+        logout: (state)=>{
+            state.status = false,
+            state.userData = null
+        }
+    }
+})
